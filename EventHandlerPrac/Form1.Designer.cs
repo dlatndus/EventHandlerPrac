651905bda5ce525586542f27deb98e3b1fb44a1a
Sooyeon Lim <81746616+dlatndus@users.noreply.github.com>
2023-05-31 12:14:40 +0900

    타이머 ui 배치

--- EventHandlerPrac/Form1.Designer.cs
+++ EventHandlerPrac/Form1.Designer.cs
@@ -28,13 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(183, 42);
+            this.button1.Location = new System.Drawing.Point(26, 42);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(434, 202);
             this.button1.TabIndex = 0;
@@ -44,7 +51,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(183, 250);
+            this.button2.Location = new System.Drawing.Point(26, 250);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(434, 202);
             this.button2.TabIndex = 1;
@@ -52,16 +59,56 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("굴림", 30F);
+            this.label1.Location = new System.Drawing.Point(44, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 109);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(22, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(262, 202);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(475, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(302, 408);
+            this.panel1.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 534);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -70,6 +117,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- EventHandlerPrac/Form1.cs
+++ EventHandlerPrac/Form1.cs
@@ -38,5 +38,10 @@ namespace EventHandlerPrac
         {
             MessageBox.Show(e.CloseReason.ToString());
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
